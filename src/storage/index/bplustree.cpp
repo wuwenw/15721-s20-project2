6b4dfd2bc7717fb8757a1e349a44ac38a64a2df6
wuwenw <wuwenw@andrew.cmu.edu>
2020-03-09 04:11:29 +0000

    format

--- src/storage/index/bplustree.cpp
+++ src/storage/index/bplustree.cpp
@@ -1,6 +1,4 @@
 
 #include "storage/index/bplustree.h"
 #include <functional>
-namespace terrier::storage::index {
-
-}  // namespace terrier::storage::index
+namespace terrier::storage::index {}  // namespace terrier::storage::index
